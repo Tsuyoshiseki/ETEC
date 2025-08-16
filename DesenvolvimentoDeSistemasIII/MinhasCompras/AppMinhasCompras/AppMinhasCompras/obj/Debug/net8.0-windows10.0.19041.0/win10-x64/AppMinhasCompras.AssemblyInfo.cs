58eac149a97a5852b7acc22f32ad8642708f2679
Luã Seki <158064034+Tsuyoshiseki@users.noreply.github.com>
2025-08-16 16:40:10 -0300

    Atividades ETEC

--- DesenvolvimentoDeSistemasIII/MinhasCompras/AppMinhasCompras/AppMinhasCompras/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/AppMinhasCompras.AssemblyInfo.cs
+++ DesenvolvimentoDeSistemasIII/MinhasCompras/AppMinhasCompras/AppMinhasCompras/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/AppMinhasCompras.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppMinhasCompras")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0bfe65d3f71f191b40a6a8d50d3ad0ba55f4e529")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+72ca124e155c931942e5ced5dc20f15d895c9fc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppMinhasCompras")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppMinhasCompras")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
