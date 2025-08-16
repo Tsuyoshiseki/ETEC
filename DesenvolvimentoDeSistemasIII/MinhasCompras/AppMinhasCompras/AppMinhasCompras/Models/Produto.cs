21e8f2c534732a78748856063c38c00765747d21
Luã Seki <158064034+Tsuyoshiseki@users.noreply.github.com>
2025-08-16 14:34:06 -0300

    ETEC

---
+++ DesenvolvimentoDeSistemasIII/MinhasCompras/AppMinhasCompras/AppMinhasCompras/Models/Produto.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+
+namespace AppMinhasCompras.Models
+{
+    internal class Produto
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Descricao { get; set; }
+
+        public double Quantidade { get; set; }
+
+        public double Preco { get; set; }
+    }
+}
